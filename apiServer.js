a6ce1e5abc5807c7e20cd418729e448be84bfd1a
dlepke <deannalepke@gmail.com>
2023-04-09 22:38:19 -0700

    cors fix

--- apiServer.js
+++ apiServer.js
@@ -25,6 +25,13 @@ app.use(
   })
 );
 
+app.use(function (req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Cookie");
+	next();
+});
+
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
